4cedd572f94a10c0c8949487aa5480ca3c73dfa4
Fathur Walkers <fathurwalkers44@gmail.com>
2021-11-02 20:43:54 -0700

    update

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -15,7 +15,7 @@
         ​<img src="{{ asset('assets/default-avatar.jpg') }}" class="img-fluid img-thumbnail" width="200px">
     </div>
     <div class="col-sm-2 col-md-2 col-lg-2">
-        <P> 
+        <P class="text-dark"> 
             Nama Lengkap <br>
             Email <br>
             Username <br>
@@ -23,12 +23,14 @@
             Status                                
         </P>    
     </div>
-    <div class="col-sm-6 col-md-6 col-lg-6">
-        : {{ $users->login_nama }} <br>
-        : {{ $users->login_email }} <br>
-        : {{ $users->login_username }} <br>
-        : {{ $users->login_telepon }} <br>
-        : <span class="badge badge-success">{{ $users->login_status }}</span>
+    <div class="col-sm-8 col-md-8 col-lg-8">
+        <p class="text-dark">
+            : {{ $users->login_nama }} <br>
+            : {{ $users->login_email }} <br>
+            : {{ $users->login_username }} <br>
+            : {{ $users->login_telepon }} <br>
+            : <span class="badge badge-success">{{ $users->login_status }}</span>
+        </p>
     </div>
 </div>
 
