07ee66590f7281d8bdb4494d5b4532d8d2393a85
Fathur Walkers <fathurwalkers44@gmail.com>
2021-11-03 18:51:37 -0700

    update

---
+++ database/migrations/2021_11_04_014015_create_bukus_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBukusTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('buku', function (Blueprint $table) {
+            $table->id();
+            $table->string('buku_judul')->nullable();
+            $table->string('buku_kode')->nullable();
+            $table->string('buku_penerbit')->nullable();
+            $table->string('buku_penulis')->nullable();
+            $table->string('buku_tahunterbit')->nullable();
+            $table->string('buku_jumlahhalaman')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('buku');
+    }
+}
