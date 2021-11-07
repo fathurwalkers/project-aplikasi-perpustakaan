181fd8181552f192c775787a02e889555538a445
Fathur Walkers <fathurwalkers44@gmail.com>
2021-11-06 17:35:30 -0700

    update

--- resources/views/admin/daftar-buku.blade.php
+++ resources/views/admin/daftar-buku.blade.php
@@ -63,15 +63,33 @@
                                         @csrf
                                         <button class="btn btn-sm btn-primary rounded mr-1">Edit</button>
                                     </form>
-
-                                    <form action="{{ route('hapus-buku', $item->id) }}" method="POST">
-                                        @csrf
-                                        <button class="btn btn-sm btn-danger rounded">Hapus</button>
-                                    </form>
+                                    <a class="btn btn-sm btn-danger rounded" data-toggle="modal" data-target="#logoutModal{{ $item->id }}">Hapus</a>
                                 </div>
                             </div>
                         </td>
                     </tr>
+
+                    <div class="modal fade" id="logoutModal{{ $item->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">×</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">Apakah anda yakin ingin keluar dari panel ini?</div>
+                                <div class="modal-footer">
+                                    <button class="btn btn-info" type="button" data-dismiss="modal">Tidak</button>
+                                    <form action="{{ route('hapus-buku', $item->id) }}" method="POST">
+                                        @csrf
+                                        <button type="submit" class="btn btn-danger">Ya</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 @endforeach
 
             </tbody>
